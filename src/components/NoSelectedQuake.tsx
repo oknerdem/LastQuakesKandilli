dfee4f97a11e1bf5441009d32f9fd1838251a30f
Okan <nayko62@gmail.com>
2023-07-15 17:59:39 +0300

    up: small visual update

--- src/components/NoSelectedQuake.tsx
+++ src/components/NoSelectedQuake.tsx
@@ -2,7 +2,7 @@ export default function NoSelectedQuake() {
   return (
     <section className="flex flex-col justify-center items-center gap-2">
       <h3 className="font-medium text-base">Deprem seçin</h3>
-      <p className="dark:text-neutral-300 text-neutral-500 text-xs">
+      <p className="dark:text-neutral-400 text-neutral-500 text-xs">
         Lütfen detaylarını görmek istediğiniz depremi seçin
       </p>
     </section>
