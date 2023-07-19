a122e6db19d380de4bf0243c1abc412b7f1ca509
Okan <nayko62@gmail.com>
2023-07-19 19:02:58 +0300

    new: quake date detail

--- src/components/QuakesTable.tsx
+++ src/components/QuakesTable.tsx
@@ -6,17 +6,36 @@ type QuakesTableProps = {
 };
 
 function QuakesTable({ setSelected, item }: QuakesTableProps) {
+  const howLongAgo = (date: Date) => {
+    const dateNow = new Date();
+    const dateThen = new Date(date);
+    const diff = dateNow.getTime() - dateThen.getTime();
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    const minutes = Math.floor(diff / (1000 * 60));
+    return { hours, minutes };
+  };
+
+  const { hours, minutes } = howLongAgo(item.date);
+  const hoursAgo = hours >= 1 ? hours : minutes;
+  const type = hours >= 1 ? 'saat' : 'dakika';
+  const pastTime =
+    hoursAgo.toString() + ' ' + type + ' önce' === '24 saat önce'
+      ? '1 gün önce'
+      : hoursAgo.toString() + ' ' + type + ' önce';
+
   return (
     <div
       className="dark:bg-neutral-700/50 bg-neutral-200/50 rounded-md py-1.5 px-3 w-full flex justify-between items-start hover:opacity-60 active:opacity-80 duration-200 cursor-pointer"
       onClick={() => setSelected(item)}
       title={item.title}
     >
-      <span className="text-sm font-normal">
+      <span className="text-sm font-medium md:max-w-full max-w-[17rem]">
         {item.location_properties.epiCenter.name
           ? item.location_properties.epiCenter.name
-          : item.title}
+          : item.title}{' '}
+        ·<span className="font-normal ml-1">{pastTime}</span>
       </span>
+
       <span className={`${item.mag > 4.0 ? 'text-rose-500' : ''}`}>
         {item.mag}
       </span>
