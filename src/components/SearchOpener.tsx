a0c43e676e30afc14cd5c609995c9c864eb63561
Okan <nayko62@gmail.com>
2023-07-20 16:38:28 +0300

    up: better size and center

--- src/components/SearchOpener.tsx
+++ src/components/SearchOpener.tsx
@@ -10,9 +10,9 @@ export default function SearchOpener({ setIsOpen }: SearchOpenerProps) {
   return (
     <motion.div
       className="absolute top-2 left-1/2 transform -translate-x-1/2"
-      initial={{ opacity: 0, y: -20 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: -20 }}
+      initial={{ opacity: 0, y: -20, x: '-50%' }}
+      animate={{ opacity: 1, y: 0, x: '-50%' }}
+      exit={{ opacity: 0, y: -20, x: '-50%' }}
       transition={{ duration: 0.4 }}
     >
       <button
@@ -21,7 +21,7 @@ export default function SearchOpener({ setIsOpen }: SearchOpenerProps) {
         title="Arama menüsünü aç"
       >
         <span className="dark:text-neutral-400 text-neutral-600">
-          <BiChevronDown />
+          <BiChevronDown size={20} />
         </span>
       </button>
     </motion.div>
